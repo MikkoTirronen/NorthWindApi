c8bbb998a9f43041bd32c177fea1ce4ed334afe9
MikkoTirronen <MikkoATirronen@gmail.com>
2025-10-29 21:27:10 +0100

    added customer service

---
+++ Services/CustomerService.cs
@@ -0,0 +1,36 @@
+using NorthWindApi.Repositories;
+using NorthWindApi.Dtos;
+namespace NorthWindApi.Services;
+
+public class CustomerService(ICustomerRepository repository)
+{
+    private readonly ICustomerRepository _repository = repository;
+
+    public async Task<IEnumerable<CustomerDto>> GetAllCustomersAsync()
+    {
+        var customers = await _repository.GetAllAsync();
+
+        return customers.Select(c => new CustomerDto
+        {
+            CustomerId = c.CustomerId,
+            CompanyName = c.CompanyName,
+            ContactName = c.ContactName,
+            Country = c.Country
+        });
+    }
+
+    public async Task<CustomerDto?> GetCustomerByIdAsync(string id)
+    {
+        var customer = await _repository.GetByIdAsync(id);
+
+        if (customer == null) return null;
+
+        return new CustomerDto
+        {
+            CustomerId = customer.CustomerId,
+            CompanyName = customer.CompanyName,
+            ContactName = customer.ContactName,
+            Country = customer.Country
+        };
+    }
+}

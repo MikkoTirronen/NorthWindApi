5864e694808e37734521d47eda2b78a6a9f6191d
MikkoTirronen <MikkoATirronen@gmail.com>
2025-10-29 21:27:30 +0100

    added customer controller

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,19 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using NorthWindApi.Repositories;
+using NorthWindApi.Services;
 
 namespace NorthWindApi.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
 
-public class CustomerController(ICustomerRepository repository) : ControllerBase
+public class CustomerController(CustomerService service) : ControllerBase
 {
-    private readonly ICustomerRepository _repository = repository;
+    private readonly CustomerService _service = service;
 
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        var customers = await _repository.GetAllAsync();
+        var customers = await _service.GetAllCustomersAsync();
         return Ok(customers);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(string id)
+    {
+        var customer = await _service.GetCustomerByIdAsync(id);
+        if (customer == null) return NotFound();
+
+        return Ok(customer);
+
     }
 }

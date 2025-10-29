11fbb45a8eef3482f076841b265d8d578ad412c3
MikkoTirronen <MikkoATirronen@gmail.com>
2025-10-29 12:51:24 +0100

    added Customer

---
+++ Models/Customer.cs
@@ -0,0 +1,10 @@
+namespace NorthWindApi.Models;
+using NorthWindApi.Models;
+
+public class Customer : Person
+{
+    public string? CustomerId { get; set; }
+    public string? ContactTitle { get; set; }
+    public string? CompanyName { get; set; }
+
+}

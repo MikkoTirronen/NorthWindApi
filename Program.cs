643577b9a9959e75acee830402a38e1e6381c9f3
MikkoTirronen <MikkoATirronen@gmail.com>
2025-10-29 21:28:32 +0100

    implemented repositories and service

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 using NorthWindApi.Repositories;
+using NorthWindApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 
+
 // Register repository (DI)
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<CustomerService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
